35ec53cd931d9008f2c7195900afc8a1e8c42227
xXDCXx Blarg <nathanbludworth@gmail.com>
2021-05-06 16:53:51 -0500

    putting-it-together-assignment

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard name = {"Doe, Jane"} age = {"45"} hair = {"Black"}/>
-      <PersonCard name = {"Smith, John"} age = {"88"} hair = {"Brown"}/>
-      <PersonCard name = {"Fillmore, Millard"} age = {"50"} hair = {"Brown"}/>
-      <PersonCard name = {"Smith, Maria"} age = {"62"} hair = {"Brown"}/>
+      <PersonCard name = {"Doe, Jane"} age = {45} hair = {"Black"}/>
+      <PersonCard name = {"Smith, John"} age = {88} hair = {"Brown"}/>
+      <PersonCard name = {"Fillmore, Millard"} age = {50} hair = {"Brown"}/>
+      <PersonCard name = {"Smith, Maria"} age = {62} hair = {"Brown"}/>
     </div>
   );
 }
--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            ageState: props.age
+        }
+    }
     render(){
         console.log("*********",this.props);
-        const {name, age, hair} = this.props;
-        return<div>
+        let {name, age, hair} = this.props;
+
+        const ageButton = () =>{
+            this.setState({ageState: this.state.ageState+1})
+            console.log("Clicked age button, this is the ages variable -->", age)
+        }
+        return(
+        <div>
             <h1>{name}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {this.state.ageState}</p>
             <p>Hair Color:  {hair}</p>
-            
+            <p><button className="btn-success" onClick={ageButton}>Birthday Button for {name}</button></p>
         </div>
+        )
     }
 }
 export default PersonCard;
